fec5269813073b14db057e6eaa5cf2492ad48fdd
Jim Blaney <jb38@hood.edu>
2015-05-04 19:07:31 -0400

    initial site impl

---
+++ site/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Database Project</title>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">  
+  </head>
+  <body>
+    
+    <div class="container">
+      <!-- content -->
+    </div>
+    
+    <script src="vendor/jquery/jquery.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.js"></script>
+  </body>
+</html> 
