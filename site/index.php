fc28c4bbd20bca2d88751285ee07ec54d60abe28
Jim Blaney <jb38@hood.edu>
2015-05-04 19:12:36 -0400

    every page should have this structure

--- site/index.php
+++ site/index.php
@@ -2,10 +2,18 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Database Project</title>
+    <title>Transportation Website | CS329 Database Project</title>
     <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">  
   </head>
   <body>
+    
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">Transportation Website</a>
+        </div>
+      </div>
+    </nav>
     
     <div class="container">
       <!-- content -->
@@ -13,5 +21,6 @@
     
     <script src="vendor/jquery/jquery.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.js"></script>
+    
   </body>
 </html> 
