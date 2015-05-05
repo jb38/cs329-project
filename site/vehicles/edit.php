bea21432efca8657f6dd71fec7405ef51f3a3162
Jim Blaney <jb38@hood.edu>
2015-05-04 20:09:09 -0400

    adfs

--- site/vehicles/edit.php
+++ site/vehicles/edit.php
@@ -26,48 +26,50 @@
     </nav>
     
     <div class="container">
-      
-      <h1>Edit Vehicle</h1>
-      
-      <form class="form form-horizontal" method="POST" action="edit.php">
+      <div class="col-sm-12">
         
-          <?php
+        <h1>Edit Vehicle</h1>
+        
+        <form class="form form-horizontal" method="POST" action="edit.php">
+          
+            <?php
+              
+              $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+              
+              $vehicle_id = $_GET["id"];
+              
+              $sql = "select v.id, v.capacity, v.type, t.description from vehicle v, vehicle_type t where v.type = t.id and v.id = " . $vehicle_id;
+   
+              $stmt = $conn->query($sql);
+              $stmt->setFetchMode(PDO::FETCH_NUM);
+  
+              $row = $stmt->fetch();
+            ?>
+            	
+            <input type="hidden" name="id" value="<?php echo($row[0]); ?>">
             
-            $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+            <div class="form-group">
+              <label>ID</label>
+              <p class="form-control-static"><?php echo($row[0]); ?></p>
+            </div>
             
-            $vehicle_id = $_GET["id"];
+            <div class="form-group">
+              <label>Capacity</label>
+              <input class="form-control" name="capacity" value="<?php echo($row[1]); ?>">
+            </div>
             
-            $sql = "select v.id, v.capacity, v.type, t.description from vehicle v, vehicle_type t where v.type = t.id and v.id = " . $vehicle_id;
- 
-            $stmt = $conn->query($sql);
-            $stmt->setFetchMode(PDO::FETCH_NUM);
-
-            $row = $stmt->fetch();
-          ?>
-          	
-          <input type="hidden" name="id" value="<?php echo($row[0]); ?>">
-          
-          <div class="form-group">
-            <label>ID</label>
-            <p class="form-control-static"><?php echo($row[0]); ?></p>
-          </div>
-          
-          <div class="form-group">
-            <label>Capacity</label>
-            <input class="form-control" name="capacity" value="<?php echo($row[1]); ?>">
-          </div>
-          
-          <!-- TODO convert this to a select -->
-          <div class="form-group">
-            <label>Type</label>
-            <input class="form-control" name="type" value="<?php echo($row[2]); ?>">
-          </div>
-          
-          <div class="form-group text-right">
-            <button class="btn btn-primary">Apply</button>
-            <a class="btn btn-link" href="index.php">Cancel</a>
-          </div>	
-      </form>
+            <!-- TODO convert this to a select -->
+            <div class="form-group">
+              <label>Type</label>
+              <input class="form-control" name="type" value="<?php echo($row[2]); ?>">
+            </div>
+            
+            <div class="form-group text-right">
+              <button class="btn btn-primary">Apply</button>
+              <a class="btn btn-link" href="index.php">Cancel</a>
+            </div>	
+        </form>
+      </div>
     </div>
     
     <script src="../vendor/jquery/jquery.js"></script>
