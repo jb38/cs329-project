921312dc0557023a0c6eac0d23969a2aefcf1f5a
Jim Blaney <jb38@hood.edu>
2015-05-06 23:16:19 -0400

    prettify values (bool)

--- site/involved_in/index.php
+++ site/involved_in/index.php
@@ -51,7 +51,7 @@
   
               while($row = $stmt->fetch()) {
                 
-                echo("<tr data-id='" . $row["vehicle_id"] . "'><td>" . $row["vehicle_id"] . "</td><td>" . $row["incident_id"] . "</td><td>" . $row["at_fault"]  . "</td><td>");
+                echo("<tr data-id='" . $row["vehicle_id"] . "'><td>" . $row["vehicle_id"] . "</td><td>" . $row["incident_id"] . "</td><td>" . ($row["at_fault"] === "1" ? "Yes" : "No")  . "</td><td>");
                 echo("<a href='edit.php?vehicle_id=" . $row["vehicle_id"] . "&incident_id=" . $row["incident_id"] . "'>Edit</a>&nbsp;");
                 echo("<a href='delete.php?vehicle_id=" . $row["vehicle_id"] . "&incident_id=" . $row["incident_id"] .  "'>Delete</a>");
                 echo("</td></tr>\n");
